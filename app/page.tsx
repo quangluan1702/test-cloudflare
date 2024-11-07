0340922eab85c31d50e29425009ab7421ea97fea
quangluan <luan.ha@stunited.vn>
2024-11-07 15:52:50 +0700

    fetch backgroud images home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
 import Image from "next/image";
 
-export default function Home() {
+interface IBackgroundImage {
+  backgroundImage:any
+  title: string;
+  subtitle: string;
+  description: string;
+  IsPublic: boolean
+};
+
+export default async function Home  () {
+  const request = await fetch(`${process.env.BASE_URL}/api/landing-activities?populate=*`, {
+    // headers: {
+    //   authorization: `Bearer ${process.env.API_TOKEN}`
+    // }
+  });  
+  const res=  await request.json()
+  const backgroundImages:IBackgroundImage[] = res.data
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello world
+      
+        {backgroundImages
+        .filter(({ IsPublic }) => IsPublic)
+        .map(({ backgroundImage, title, subtitle }) => (
+          <Image src={`${process.env.BASE_URL}${backgroundImage?.formats?.thumbnail?.url}`} key={subtitle} alt={title}  width={1000} height={1000}></Image>
+
+        ))}
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1', // Only the IP address here
+        port: '1337',          // Specify the port separately
+        pathname: '/uploads/**', // Path without leading or trailing slashes
+      },
+    ],
+  },
 };
 
 export default nextConfig;
